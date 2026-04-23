1f7101c1fd6ae9f48bd6a63de9e17d3ce73781bd
Oktay Özel <mail.oktayozel@gmail.com>
2026-04-23 14:10:02 -0400

    add hardhat config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -4,7 +4,22 @@ import "@oasisprotocol/sapphire-hardhat";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.28",
+    version: "0.8.24",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+    },
+  },
+  networks: {
+    sapphire_testnet: {
+      url: "https://testnet.sapphire.oasis.io",
+      chainId: 0x5aff,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    sapphire_localnet: {
+      url: "http://localhost:8545",
+      chainId: 0x5afd,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
   },
 };
 
